322c8eddbd429fc2ebc5dc4834b67858dac7157b
understandbasics <shreenathshenoy12@gmail.com>
2024-08-15 20:01:43 +0530

    Text Edited

--- pomodoro/lib/main.dart
+++ pomodoro/lib/main.dart
@@ -36,7 +36,7 @@ class MyHomePage extends StatelessWidget {
  Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
-       title: Text("Flutter Alert Dialog Example"),
+       title: Text("Flutter"),
      ),
      body: Center(
        child: ElevatedButton(
