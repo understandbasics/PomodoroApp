d51cb7085137d427160c435fb4689e734b98e581
shreyashettykonjadi <148181207+shreyashettykonjadi@users.noreply.github.com>
2024-08-19 16:14:04 +0530

    removing problems(not errors)

--- pomodoro/lib/NavBar.dart
+++ pomodoro/lib/NavBar.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors, avoid_returning_null_for_void
+
 import 'package:flutter/material.dart';
 
 class Navbar extends StatelessWidget {
--- pomodoro/lib/main.dart
+++ pomodoro/lib/main.dart
@@ -1,7 +1,11 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:pomodoro/NavBar.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget{
   @override
